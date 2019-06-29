0e24962c549eacef457b286b5c16a9210edd8d21
0613ryo <yr130690@gmail.com>
2019-06-29 22:49:23 +0900

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+  before_action :login_required, except: [:index, :show]
+
+  # 記事一覧
+  def index
+    @articles = Article.order(released_at: :desc)
+
+    @articles = @articles.open_to_the_public unless current_member
+
+    unless current_member&.administrator?
+      @articles = @articles.visible
+    end
+
+    @articles = @articles.page(params[:page]).per(5)
+  end
+
+  # 記事詳細
+  def show
+    articles = Article.all
+
+    articles = articles.open_to_the_public unless current_member
+
+    unless current_member&.administrator?
+      articles = articles.visible
+    end
+
+    @article = @articles.find(params[:id])
+  end
+end
